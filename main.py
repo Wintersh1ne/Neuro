7e9be6476f69b72dbadd73b3fec57ed28b86d5a9
Wintersh1ne <130225456+Wintersh1ne@users.noreply.github.com>
2023-07-07 19:52:08 +0300

    j
    какой-то рофл в левых инпутах

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import PySimpleGUI as g
-
+import numpy as np
 import neuro
 
 ic = 'mai.ico'
@@ -7,13 +7,13 @@ ic = 'mai.ico'
 g.theme('default1')
 
 layout_1 = [[g.Text('ПРИМЕР РАБОТЫ НЕЙРОНА', justification='center', expand_x=True, font='Impact 30 normal')],
-            [g.Input('2', enable_events=True, key='-INPUT1-', tooltip='Например 2', size=(13, 1), justification='center', pad=(213, 5))],
+            [g.Input('0', size=(13, 1), justification='center', key='Input_1', enable_events=True),
+             g.Input('2', enable_events=True, key='-INPUT1-', tooltip='Например 2', size=(13, 1), justification='center', pad=(113, 5))],
             [g.Image(source='neuron.png')],
-            [g.Input('3', enable_events=True, key='-INPUT2-', tooltip='Например 3', size=(13, 1), justification='center', pad=(213, 5))],
+            [g.Input('1', enable_events=True, key='Input_2', size=(13, 1), justification='center'),
+             g.Input('3', enable_events=True, key='-INPUT2-', tooltip='Например 3', size=(13, 1), justification='center', pad=(113, 5))],
             [g.Button('Посчитать', enable_events=True, key='-COUNT-',), g.Output(expand_x=True)],
-            [g.StatusBar('НИЖНИЙ ТЕКСТ', justification='center', expand_x=True, font='Impact 30 normal')],
-            [g.Input('0', size=(13, 1), justification='left', key='Input_1', enable_events=True)],
-            [g.Input('1', size=(13, 1), justification='left', key='Input_2', enable_events=True)]]
+            [g.StatusBar('НИЖНИЙ ТЕКСТ', justification='center', expand_x=True, font='Impact 30 normal')]]
 
 layout_2 = [[g.Text('ПРИМЕР РАБОТЫ НЕЙРОСЕТИ', justification='center', expand_x=True, font='Impact 30 normal')],
             [g.Image(source='image.png')],
@@ -27,24 +27,24 @@ print('Результат', end="")
 while True:
     event, values = window.read()
     if event == g.WIN_CLOSED:
-        break
+        exit(0)
     if event == '-INPUT1-':
-        if values['-INPUT1-'][-1] not in ('0123456789'):
+        if values['-INPUT1-'] != None and values['-INPUT1-'][-1] not in ('0123456789'):
             g.popup("Only digits allowed")
             window['-INPUT1-'].update(values['-INPUT1-'][:-1])
     if event == '-INPUT2-':
-        if values['-INPUT2-'][-1] not in ('0123456789'):
+        if values['-INPUT2-'] != None and values['-INPUT2-'][-1] not in ('0123456789'):
             g.popup("Only digits allowed")
             window['-INPUT2-'].update(values['-INPUT2-'][:-1])
     if event == 'Input_1':
-        if values['Input_1'][-1] not in ('0123456789'):
+        if values['-Input_1-'] != None and values['Input_1'][-1] not in ('0123456789'):
             g.popup("Only digits allowed")
             window['Input_1'].update(values['Input_1'][:-1])
     if event == 'Input_2':
-        if values['Input_2'][-1] not in ('0123456789'):
+        if values['-Input_2-'] != None and values['Input_2'][-1] not in ('0123456789'):
             g.popup("Only digits allowed")
             window['Input_2'].update(values['Input_2'][:-1])
-    if event == 'COUNT-':
-        True
-    print('\nРезультат: ', end="")
+    if event == '-COUNT-':
+        n = neuro.Neuron(np.array([int(values['Input_1']), int(values['Input_2'])]), 4)
+        print('\nРезультат: ', n.feedforward(np.array([int(values['-INPUT1-']), int(values['-INPUT2-'])])), end="")
 window.close()

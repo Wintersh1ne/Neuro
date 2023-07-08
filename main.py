6553a3f96b510c237b391c114d96a745aebdfaec
Wintersh1ne <130225456+Wintersh1ne@users.noreply.github.com>
2023-07-08 13:29:33 +0300

    r

--- main.py
+++ main.py
@@ -29,23 +29,25 @@ while True:
     if event == g.WIN_CLOSED:
         break
     if event == '-INPUT1-':
-        if values['-INPUT1-'] == None or values['-INPUT1-'][-1] not in ('0123456789'):
+        if values['-INPUT1-'] != "" and (values['-INPUT1-'][-1] not in ('0123456789') or values['-INPUT1-'][0] not in ('0123456789')):
             g.popup("Only digits allowed")
             window['-INPUT1-'].update(values['-INPUT1-'][:-1])
+
     if event == '-INPUT2-':
-        if values['-INPUT2-'] == "" or values['-INPUT2-'][-1] not in ('0123456789'):
+        print(values['-INPUT2-'][1:])
+        if values['-INPUT2-'] != "" and (values['-INPUT2-'][-1] not in ('0123456789') or values['-INPUT2-'][0] not in ('0123456789')):
             g.popup("Only digits allowed")
             window['-INPUT2-'].update(values['-INPUT2-'][:-1])
     if event == 'Input_1':
-        if values['-Input_1-'] == "" or values['Input_1'][-1] not in ('0123456789'):
+        if values['Input_1'] != "" and (values['Input_1'][-1] not in ('0123456789') or values['Input_1'][0] not in ('0123456789')):
             g.popup("Only digits allowed")
             window['Input_1'].update(values['Input_1'][:-1])
     if event == 'Input_2':
-        if values['-Input_2-'] == "" or values['Input_2'][-1] not in ('0123456789'):
+        if values['Input_2'] != "" and (values['Input_2'][-1] not in ('0123456789') or values['Input_2'][0] not in ('0123456789')):
             g.popup("Only digits allowed")
             window['Input_2'].update(values['Input_2'][:-1])
     if event == '-COUNT-':
-        n = neuro.Neuron(np.array([int(values['Input_1']), int(values['Input_2'])]), 4)
-        print('\nРезультат: ', n.feedforward(np.array([int(values['-INPUT1-']), int(values['-INPUT2-'])])), end="")
+        n = neuro.Neuron(np.array([int(layout_1[1][0].get()), int(layout_1[3][0].get())]), 4)
+        print('\nРезультат: ', n.feedforward(np.array([int(layout_1[1][1].get()), int(layout_1[3][1].get())])), end="")
 window.close()
 exit(0)

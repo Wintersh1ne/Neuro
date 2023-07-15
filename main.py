4e9d0492355f46069a411a6ed96f58f1cc72a4aa
Wintersh1ne <130225456+Wintersh1ne@users.noreply.github.com>
2023-07-16 00:28:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,10 +22,12 @@ layout_2 = [[g.Text('ПРИМЕР РАБОТЫ НЕЙРОСЕТИ', justificatio
             [g.FileBrowse('Выбрать файл'), g.FileSaveAs('Сохранить как'), g.Button('Настроить', enable_events=True, key='Parametrize')]]
 
 layout_3 = [[g.Text('ПАРАМЕТРЫ НЕЙРОСЕТИ', justification='center', expand_x=True, font='Impact 30 normal')],
-            [g.Text('x: '), g.Input('0', enable_events=True, key='Input_x', tooltip='Например 0', size=(5, 1), justification='center'), g.Text('weight 1: '), g.Input('2', enable_events=True, key='Input_weight_x_1', tooltip='Например 2', size=(15, 1), justification='center'), g.Text('weight 3: '), g.Input('2', enable_events=True, key='Input_weight_x_2', tooltip='Например 2', size=(15, 1), justification='center')],
-            [g.Text('y: '), g.Input('1', enable_events=True, key='Input_y', tooltip='Например 1', size=(5, 1), justification='center'), g.Text('weight 2: '), g.Input('3', enable_events=True, key='Input_weight_y_1', tooltip='Например 3', size=(15, 1), justification='center'), g.Text('weight 4: '), g.Input('2', enable_events=True, key='Input_weight_y_2', tooltip='Например 2', size=(15, 1), justification='center')],
-            [],
-            [g.Button('Return', enable_events=True, key='return')]]
+            [g.Text('x:   '), g.Input('0', enable_events=True, key='Input_x', tooltip='Например 0', size=(5, 1), justification='center'), g.Text('weight 1: '), g.Input('2', enable_events=True, key='Input_weight_x_1', tooltip='Например 2', size=(15, 1), justification='center'), g.Text('weight 3: '), g.Input('2', enable_events=True, key='Input_weight_x_2', tooltip='Например 2', size=(15, 1), justification='center')],
+            [g.Text('y:   '), g.Input('1', enable_events=True, key='Input_y', tooltip='Например 1', size=(5, 1), justification='center'), g.Text('weight 2: '), g.Input('3', enable_events=True, key='Input_weight_y_1', tooltip='Например 3', size=(15, 1), justification='center'), g.Text('weight 4: '), g.Input('2', enable_events=True, key='Input_weight_y_2', tooltip='Например 2', size=(15, 1), justification='center')],
+            [g.Text('b1: '), g.Input('1', enable_events=True, key='Input_b1', tooltip='Например 1', size=(5, 1), justification='center'), g.Text('weight 5: '), g.Input('3', enable_events=True, key='Input_weight_b1', tooltip='Например 3', size=(15, 1), justification='center')],
+            [g.Text('b2: '), g.Input('1', enable_events=True, key='Input_b2', tooltip='Например 1', size=(5, 1), justification='center'), g.Text('weight 6: '), g.Input('3', enable_events=True, key='Input_weight_b2', tooltip='Например 3', size=(15, 1), justification='center')],
+            [g.Text('b3: '), g.Input('1', enable_events=True, key='Input_b3', tooltip='Например 1', size=(5, 1), justification='center')],
+            [g.Button('Save and return', enable_events=True, key='save_and_return')]]
 
 layout_0 = [[g.TabGroup([[g.Tab('Нейрон', layout_1), g.Tab('Сеть', layout_2)]])]]
 window = g.Window('Нейросети', layout_0, finalize=True, icon=ic)
@@ -60,11 +62,16 @@ while True:
         window_params.force_focus()
         while True:
             event_p, values_p = window_params.read()
-            if event_p == 'return':
-                window_params.disappear()
-                window.enable()
+            if event_p == 'save_and_return':
+                try:
+                    window_params.disappear()
+                    window.enable()
+                    n = neuro.Neuron(np.array([int(layout_1[1][0].get()), int(layout_1[3][0].get())]), int(layout_1[2][0].get()))
+                    print('\nРезультат: ', n.feedforward(np.array([int(layout_1[1][1].get()), int(layout_1[3][1].get())])), end="")
+                except:
+                    g.popup("Check all parametres. They must be digitals for sure!")
                 break
-            if event_p in ['Input_x', 'Input_y', 'Input_weight_x_1', 'Input_weight_x_2', 'Input_weight_y_1', 'Input_weight_y_2']:
+            if event_p in ['Input_x', 'Input_y', 'Input_b1', 'Input_b2', 'Input_b3', 'Input_weight_x_1', 'Input_weight_x_2', 'Input_weight_y_1', 'Input_weight_y_2', 'Input_weight_b1', 'Input_weight_b2']:
                 if len(values_p[event_p]) == 1 and values_p[event_p][0] not in ('0123456789'):
                     g.popup("Only digits allowed")
                     window_params[event_p].update("")
